f81949363c968e46bdd7aea63acfdef11e5c6811
Wellinton Alves <wellintonalves1910@gmail.com>
2026-08-12 21:29:16 -0300

    chore: setup de ambiente local para testes no celular
    - Habilitado vite --host
    
    - Ajustado CORS e baseURL para permitir acesso via IP da rede local

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,14 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1 || origin.match(/^http:\/\/(192\.168\.|10\.|172\.(1[6-9]|2[0-9]|3[0-1])\.|localhost)/)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
