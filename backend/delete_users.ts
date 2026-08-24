8173b31ea93b0b62525c027083edcfc88bf78112
Wellinton Alves <wellintonalves1910@gmail.com>
2026-08-24 20:27:54 -0300

    chore: remove scripts antigos e rascunhos

--- backend/delete_users.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.usuario.deleteMany({ where: { emailVerificado: false } })
-  .then(r => { console.log('Deletados:', r.count); prisma.$disconnect(); })
-  .catch(e => { console.error(e); prisma.$disconnect(); });
--- backend/test_prisma.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from './src/lib/prisma';
-prisma.barbeiro.findMany().then(b => {
-  console.log('Barbeiros: ', b.length);
-  console.log(b);
-}).catch(console.error).finally(() => process.exit(0));
